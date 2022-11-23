a6ea8a0c0c9e6a5ded6abaefdc49b64c16506bde
the-future-warrior <saptakmanna17@gmail.com>
2022-11-23 23:02:52 +0530

    Initial Commit...

---
+++ app/src/main/java/com/mr_17/nvironment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mr_17.nvironment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/mr_17/nvironment/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.mr_17.nvironment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
