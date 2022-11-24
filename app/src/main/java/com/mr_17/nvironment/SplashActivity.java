b70bd3de4895c62b63f23c88396ea3340c466a30
the-future-warrior <saptakmanna17@gmail.com>
2022-11-24 11:55:00 +0530

    Splash Activity added...

--- app/src/main/java/com/mr_17/nvironment/SplashActivity.java
+++ app/src/main/java/com/mr_17/nvironment/SplashActivity.java
@@ -2,7 +2,10 @@ package com.mr_17.nvironment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run()
+            {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
