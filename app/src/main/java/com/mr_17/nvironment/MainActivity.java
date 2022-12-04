a3db2bd6646f2cea3bf047a1736e992ea5ef1be6
the-future-warrior <saptakmanna17@gmail.com>
2022-12-04 15:46:07 +0530

    Home page UI updated...

--- app/src/main/java/com/mr_17/nvironment/MainActivity.java
+++ app/src/main/java/com/mr_17/nvironment/MainActivity.java
@@ -1,19 +1,28 @@
 package com.mr_17.nvironment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth myAuth;
-    private Button postButton, contributeButton, supportUsButton, logoutButton;
+    private androidx.appcompat.widget.Toolbar toolbar;
+    private CardView postButton, contributeButton, supportUsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,23 +51,48 @@ public class MainActivity extends AppCompatActivity {
                 SendToActivity(PaymentActivity.class, true);
             }
         });
-
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myAuth.signOut();
-                SendToActivity(LoginActivity.class, false);
-            }
-        });
     }
 
     private void InitializeFields()
     {
         myAuth = FirebaseAuth.getInstance();
-        postButton = findViewById(R.id.post_button);
-        contributeButton = findViewById(R.id.contribute_button);
-        supportUsButton = findViewById(R.id.support_us_button);
-        logoutButton = findViewById(R.id.logout_button);
+        postButton = findViewById(R.id.post_activity_card);
+        contributeButton = findViewById(R.id.contribute_card);
+        supportUsButton = findViewById(R.id.support_us_card);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(new androidx.appcompat.widget.Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.home_nav_log_out)
+                {
+                    // setting up the alert dialog
+                    final AlertDialog.Builder confirmLogoutDialog = new AlertDialog.Builder(MainActivity.this);
+                    confirmLogoutDialog.setTitle("Confirm Logout");
+                    confirmLogoutDialog.setMessage("Are sure you want to logout?");
+
+                    // creating functionality of the "yes" button
+                    confirmLogoutDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            myAuth.signOut();
+                            SendToActivity(LoginActivity.class, false);
+                        }
+                    });
+
+                    // creating the "no" button
+                    confirmLogoutDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+                    confirmLogoutDialog.create().show();
+                }
+                return true;
+            }
+        });
     }
 
     private void SendToActivity(Class<? extends Activity> activityClass, boolean backEnabled)
